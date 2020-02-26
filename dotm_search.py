27f656ef0f0f8adf976491f1f2b609a0e693e199
Chelsea <chewh115@gmail.com>
2020-02-26 09:11:50 -0500

    Sets up argparse command line arguments

--- dotm_search.py
+++ dotm_search.py
@@ -5,7 +5,24 @@
 Given a directory path, search all files in the path for a given text string
 within the 'word/document.xml' section of a MSWord .dotm file.
 """
-__author__ = "???"
+__author__ = "chewh115"
+
+import argparse
+import os
+
+
+parser = argparse.ArgumentParser(
+    description="Searches files in a directory for given keyword")
+
+parser.add_argument('search-term', action="store",
+                    help="Keyword you want to search for.")
+parser.add_argument('--directory', 'path', action="store", default='./',
+                    help="The directory to search in. Default is current directory.")
+
+
+def get_files(path):
+    for root, dirs, files in (path, topdown=False):
+        pass
 
 
 def main():
