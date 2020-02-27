ff9fcef7244238e4373c8d389bb88de6a2f6055b
Chelsea <chewh115@gmail.com>
2020-02-26 19:18:00 -0500

    Rearranges code for my own sanity, utilizes ability to create path for files and has working solution

--- dotm_search.py
+++ dotm_search.py
@@ -5,7 +5,7 @@
 Given a directory path, search all files in the path for a given text string
 within the 'word/document.xml' section of a MSWord .dotm file.
 """
-__author__ = "chewh115"
+__author__ = "chewh115, and I talked it out with Janell.Huyck while coding."
 
 import argparse
 import os
@@ -24,29 +24,50 @@ def create_parser():
     return search_info
 
 
-def get_dotm_files(path):
+def get_dotm_files(directory):
     file_list = []
-    for (root, dirs, files) in os.walk(path):
+    for (root, dirs, files) in os.walk(directory):
         for file in files:
             if file.endswith('.dotm'):
-                file_list.append(os.path.join(root, file)
+                file_list.append(os.path.join(root, file))
     return file_list
 
 
-def unzip_file(files):
-    unzipped_files=[]
-    for file in files:
-        with zipfile.Zipfile(file, 'r') as zip:
-            unzipped_files.append(zip.extract('word/document.xml'))
-    return unzipped_files
+def unzip_file(directory, search_term):
+    file_list = get_dotm_files(directory)
+    search_term_counter = 0
+    for file in file_list:
+        with zipfile.ZipFile(file, 'r') as zipped_files:
+            zipped_files.extractall('./extracted')
+        with open('./extracted/word/document.xml', 'r') as file_to_search:
+            file_words = file_to_search.read()
+            search_context = search_for_term(file_words, search_term)
+            if search_context != -1:
+                print('Match found in: {}'.format(file))
+                print('The context is: {}'.format(search_context))
+                search_term_counter += 1
+    print('Number of files searched for {}: '.format(
+        search_term) + str(len(file_list)))
+    print('We found {} files containing {}.'.format(
+        search_term_counter, search_term))
+
+
+def search_for_term(file_words, search_term):
+    term_index = file_words.find(search_term)
+    context_slice = min(len(file_words), (term_index + 40))
+    if term_index == -1:
+        return -1
+    elif term_index < 40:
+        return file_words[0:context_slice]
+    else:
+        return file_words[term_index - 40:context_slice]
 
 
 def main():
-    search_info=create_parser()
-    directory=search_info.directory
-    search_term=search_info.search_term
-    file_list=get_files(directory)
-    unzipped_files=unzip_files(file_list)
+    search_info = create_parser()
+    directory = search_info.directory
+    search_term = search_info.search_term
+    unzip_file(directory, search_term)
 
 
 if __name__ == '__main__':
